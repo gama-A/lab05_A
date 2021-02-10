1028ede35de0ec9728377e8275beb7feb7262ce3
Gama Aristondo <gamaliel@cs.ucsb.edu>
2021-02-09 21:12:46 -0800

    loop not working

--- print_array.asm
+++ print_array.asm
@@ -4,17 +4,46 @@
 # Don't forget to:
 #   make all arguments to any function go in $a0 and/or $a1
 #   make all returned values from functions go in $v0
+# Gama Aristondo
 
 .data
-	# TODO: Write your initializations here
+	array: .word 1 2 3 4 5 6 7 8 9 10
+    string: .asciiz "The contents of the array are:\n"
+    newline: .asciiz "\n"
 
 .text
 printA:
-    # TODO: Write your function code here
+    
+    li $t0, 0  # variable i in the for loop
+    move $t4, $a0  #$t4 will hold address since a0 is needed to print number and string
+
+printA_loop:
+
+    slt $t1, $t0, $a1
+    beq $t1, $zero, printA_exit
+    sll $t1, $t0, 2
+    add $t4, $t4, $t1
+    lw $t2, 0($t4)
+
+    li $v0, 1
+    move $a0, $t2
+    syscall
+    li $v0, 4
+    la $a0, newline
+    syscall
+
+    addi $t0, $t0, 1
+
+printA_exit:
+
+    jr $ra
 
 main:
 
-	# TODO: Write your main function code here
+    la $a0, array
+	li $a1, 10
+    jal printA
 
 exit:
-	# TODO: Write code to properly exit a SPIM simulation
+	li $v0, 10
+    syscall
